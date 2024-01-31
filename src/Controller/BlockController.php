6d15d376879fe55907233271f3531d3f5a5c8c38
l-Kopylov <qazar7@yandex.ru>
2024-02-01 00:53:29 +0300

    создание интерфейса твиг

--- src/Controller/BlockController.php
+++ src/Controller/BlockController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Parts;
+use App\Repository\CommentRepository;
+use App\Repository\PartsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
 
 class BlockController extends AbstractController{
     #[Route('/', name: 'homepage')]
-    public function index(): Response{
-    
-        return new Response ( <<<EOF
-            <html>
-                <body>
-                    <img src="/images/profilactica.gif" />
-                </body>
-           </html>
-           EOF);
+    public function index(Environment $twig, PartsRepository $partsRepository): Response
+    {
+            return new Response($twig->render('parts/index.html.twig', [
+                    'parts' => $partsRepository->findAll(),
+                ]));
+    }
+
+        #[Route('/parts/{id}', name: 'parts')]
+    public function show(Environment $twig, Parts $parts, CommentRepository $commentRepository): Response
+    {
+        return new Response($twig->render('parts/show.html.twig', [
+            'parts' => $parts,
+            'comments' => $commentRepository->findBy(['parts' => $parts], ['createdAt' => 'DESC']),
+        ]));
     }
 }
