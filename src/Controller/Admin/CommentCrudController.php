49872a47320984a3e2ac65178a8915d9c1a27173
l-Kopylov <qazar7@yandex.ru>
2024-02-02 22:56:13 +0300

    ORM\PrePersist createdat

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -50,9 +50,7 @@ class CommentCrudController extends AbstractCrudController
         ]);
         if (Crud::PAGE_EDIT === $pageName) {
             yield $createdAt->setFormTypeOption('disabled', true);
-        } else {
-            yield $createdAt;
-        }
+        } 
     }
 
 }
